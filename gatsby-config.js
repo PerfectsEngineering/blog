3638972f0116d1649d9d06677b73251a87859dc9
Perfect Makanju <damiperfect@gmail.com>
2019-01-21 01:47:13 +0100

    Add twitter config

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,7 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    `gatsby-plugin-netlify-cms`
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-twitter`
   ],
 }
