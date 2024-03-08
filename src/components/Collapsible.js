a2ed1f570edfed27429e680a679e7b0547b0a5cf
Perfect Makanju <damiperfect@gmail.com>
2024-03-08 15:02:59 +0000

    docs: complete article content

--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -3,24 +3,31 @@ import { Collapse } from 'antd'
 
 const { Panel } = Collapse
 
-export const Collapsible = ({ title, expanded, children }) => {
+export const Collapsible = ({ title, expanded, children, className }) => {
   const [expanedState, setExpandedState] = useState(expanded)
-  const toggleExpandedState = useCallback((selectedIndices) => {
-    setExpandedState(selectedIndices.length > 0)
-  }, [expanedState]);
+  const toggleExpandedState = useCallback(
+    (selectedIndices) => {
+      setExpandedState(selectedIndices.length > 0)
+    },
+    [expanedState]
+  )
 
   const titleElement = (
     <div>
       {title}
       &nbsp;
-      <br/>
+      <br />
       <small>
         <em>Tap to {expanedState ? 'collapse' : 'expand'}</em>
       </small>
     </div>
   )
   return (
-    <Collapse defaultActiveKey={expanded ? '1' : null} onChange={toggleExpandedState}>
+    <Collapse
+      defaultActiveKey={expanded ? '1' : null}
+      onChange={toggleExpandedState}
+      className={className}
+    >
       <Panel header={titleElement} key="1">
         {children}
       </Panel>
