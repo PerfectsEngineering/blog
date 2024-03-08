2aab48aca85e2c7ae5c6fac183e6cb83c625d296
Perfect Makanju <damiperfect@gmail.com>
2024-03-08 11:56:08 +0000

    feat(collapsible): include prompt to tap to expand a value

--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -1,13 +1,29 @@
-import React from "react";
-import { Collapse } from 'antd';
+import React, { useCallback, useState } from 'react'
+import { Collapse } from 'antd'
 
 const { Panel } = Collapse
 
-
 export const Collapsible = ({ title, expanded, children }) => {
-  return <Collapse defaultActiveKey={expanded ? "1" : null}>
-    <Panel header={title} key="1">
-      {children}
-    </Panel>
-  </Collapse>;
+  const [expanedState, setExpandedState] = useState(expanded)
+  const toggleExpandedState = useCallback((selectedIndices) => {
+    setExpandedState(selectedIndices.length > 0)
+  }, [expanedState]);
+
+  const titleElement = (
+    <div>
+      {title}
+      &nbsp;
+      <br/>
+      <small>
+        <em>Tap to {expanedState ? 'collapse' : 'expand'}</em>
+      </small>
+    </div>
+  )
+  return (
+    <Collapse defaultActiveKey={expanded ? '1' : null} onChange={toggleExpandedState}>
+      <Panel header={titleElement} key="1">
+        {children}
+      </Panel>
+    </Collapse>
+  )
 }
